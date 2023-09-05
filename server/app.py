43cc2bfd156d1450a5565444d3a742601271b1fc
Denzel <jdenzelmab@gmail.com>
2023-09-05 15:54:19 -0400

    Completed flask sqlalchemy lab

--- server/app.py
+++ server/app.py
@@ -19,15 +19,50 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+
+    response = f'''
+        <ul>Name: {animal.name}</ul>
+        <ul>Species: {animal.species}</ul>
+        <ul>Zookeeper: {animal.zookeeper.name}</ul>
+        <ul>Enclosure: {animal.enclosure.environment}</ul>
+        
+    '''
+    return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+
+    animals_list = ""
+    for animals in zookeeper.animals:
+        animals_list += f'<ul>Animal: {animals.name}</ul>' 
+        
+    response = f'''
+        <ul>ID: {Zookeeper.id}</ul>
+        <ul>Name: {zookeeper.name}</ul>
+        <ul>Birthday: {zookeeper.birthday}</ul>
+        {animals_list}
+    '''
+        
+
+    return response
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+
+    animals_list = ""
+    for animals in enclosure.animals:
+        animals_list += f'<ul>Animal: {animals.name}</ul>'
+
+    response = f'''
+        <ul>ID: {enclosure.id}</ul>
+        <ul>Environment: {enclosure.environment}</ul>
+        <ul>Open to Visitors: {enclosure.open_to_visitors}</ul>
+        {animals_list}
+    '''
+    return response
 
 
 if __name__ == '__main__':
